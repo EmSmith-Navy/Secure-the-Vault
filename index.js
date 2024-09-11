447b93f40acb0c257df2791742a54cff80478c0d
EmSmith-Navy <keino2586@gmail.com>
2024-09-11 14:50:21 -0500

    added files

---
+++ index.js
@@ -0,0 +1,22 @@
+// You will be using +, - and * to solve the Vault
+// In the Order listed below
+// * multiply to get 10
+// - subtract to get 40
+// + add to get 39
+alert("10 - 40 -39");
+
+const five = 5;
+const seven = 7;
+
+console.log(five * seven);
+
+const sixty = 60;
+const twenty = 20;
+
+console.log(sixty - twenty);
+
+const fifteen = 15
+const twentyFour =24
+
+console.log(fifteen + twentyFour);
+
